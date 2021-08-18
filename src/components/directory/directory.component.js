4cab89f739add4cf2a33905afd2de1e03f9df031
sdanieau8 <psdanieau@gmail.com>
2021-08-18 11:54:45 -0700

    adding header component

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -5,7 +5,7 @@ import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss';
 
 class Directory extends React.Component {
-  constructor() {
+  constructor() { 
     super();
 
     this.state = {
@@ -48,8 +48,8 @@ class Directory extends React.Component {
     return (
       <div className='directory-menu'>
         {
-          this.state.sections.map(({ title, imageUrl, id, size, linkUrl }) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+          this.state.sections.map(({ id, ...otherSectionProps }) => (
+            <MenuItem key={id} {...otherSectionProps} />
           ))}
       </div>
     );
